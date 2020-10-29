e91db4a8ace48b827d7db909d6f266d8f32bf8f0
adha setiawan <adhasetiawanwiyana@gmail.com>
2020-10-29 17:07:27 +0800

    masih tidak bisa konek

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(res,res){
-    response.ok('Aplikasi Rest dapat berjalan')
+    response.ok("Aplikasi Rest dapat berjalan", res)
 };
