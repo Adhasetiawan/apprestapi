f17eadd1488184841b7cd0a28c739b3d53a3fae4
adha setiawan <adhasetiawanwiyana@gmail.com>
2020-10-30 16:50:48 +0800

    fungsi retrieve pertama sudah jadi

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function(req,res){
     response.ok("Aplikasi Rest dapat berjalan", res)
 };
+
+//menampilkan semua data mahasiswa
+exports.tampilsemuadatamahasiswa = function (req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else{
+            response.ok(rows, res);
+        }
+    });
+};
