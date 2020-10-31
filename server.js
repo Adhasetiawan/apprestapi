b49ef1440d5558b6e4fcfe0e582b3dbdf03bff6a
adha setiawan <adhasetiawanwiyana@gmail.com>
2020-10-31 12:02:30 +0800

    registrasi sudah bisa

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const app = express();
 
+
+var morgan = require('morgan');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // panggil routes
 var routes = require('./routes');
 routes(app);
+
+//daftar menu router dari index
+app.use('/auth', require('./middleware'));
 
 app.listen(3000, () => {
     console.log(`Server started on port`);
