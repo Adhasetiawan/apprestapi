a5917dd4a3854d5cbf8823ab8ab6f1da2551cad8
adha setiawan <adhasetiawanwiyana@gmail.com>
2020-11-05 20:49:24 +0800

    asih belum bisa verifikasi dengan melakukan set roll yang sudah tersedia

--- middleware/index.js
+++ middleware/index.js
@@ -10,6 +10,6 @@ router.post('/api/v1/register', auth.registrasi);
 router.post('/api/v1/login', auth.login);
 
 // alamat dengan otoritas khusus
-router.get('/api/v1/rahasia', verifikasi(2), auth.halamanrahasia);
+router.get('/api/v1/rahasia', verifikasi(), auth.halamanrahasia);
 
 module.exports = router;
